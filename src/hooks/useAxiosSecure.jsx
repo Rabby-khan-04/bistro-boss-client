6f9380b7bf8b6b00aefb4158a34bda6349f7a5a5
Rabby-khan-04 <ajrabbyk72@gmail.com>
2025-02-22 01:34:11 +0600

    added request and response interceptors

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,5 +1,7 @@
 import axios from "axios";
 import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import useAuth from "./useAuth";
 
 const axiosSecure = axios.create({
   baseURL: "http://localhost:5000/api/v1",
@@ -7,19 +9,39 @@ const axiosSecure = axios.create({
 });
 
 const useAxiosSecure = () => {
+  const navigate = useNavigate();
+  const { logOut } = useAuth();
   useEffect(() => {
     const interceptor = axiosSecure.interceptors.request.use(
-      (config) => config,
+      (config) => {
+        const token = localStorage.getItem("access-token");
+        config.headers.authorization = `Bearer ${token}`;
+        return config;
+      },
       (err) => {
         console.log(err);
         return Promise.reject(err);
       }
     );
 
+    axios.interceptors.response.use(
+      function (response) {
+        return response;
+      },
+      async function (error) {
+        const status = error.response.status;
+        if (status === 401 || status === 403) {
+          await logOut();
+          navigate("/signin");
+        }
+        return Promise.reject(error);
+      }
+    );
+
     return () => {
       axiosSecure.interceptors.response.eject(interceptor);
     };
-  }, []);
+  }, [navigate, logOut]);
 
   return axiosSecure;
 };
