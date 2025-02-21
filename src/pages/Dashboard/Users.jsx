d9c0f181e8603242d4fbd79dd97014543cc129c1
Rabby-khan-04 <ajrabbyk72@gmail.com>
2025-02-22 00:36:32 +0600

    user delete function added

--- src/pages/Dashboard/Users.jsx
+++ src/pages/Dashboard/Users.jsx
@@ -1,12 +1,14 @@
 import SectionTitle from "@/components/shared/SectionTitle";
 import Spinner from "@/components/shared/Spinner";
 import useAxiosSecure from "@/hooks/useAxiosSecure";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { HiUserGroup } from "react-icons/hi2";
 import { RiAdminFill, RiDeleteBin5Line } from "react-icons/ri";
+import Swal from "sweetalert2";
 
 const Users = () => {
   const axiosSecure = useAxiosSecure();
+  const queryClient = useQueryClient();
 
   const { data: users = [], isLoading: userIsLoading } = useQuery({
     queryKey: ["users"],
@@ -17,7 +19,75 @@ const Users = () => {
     },
   });
 
+  const { mutateAsync: deletUser } = useMutation({
+    mutationKey: ["delete-user"],
+    mutationFn: async (id) => {
+      const res = await axiosSecure.delete(`/users/user/${id}`);
+
+      return res;
+    },
+    onSuccess: (res) => {
+      if (res.data.data.deletedCount > 0) {
+        queryClient.invalidateQueries(["users"]);
+        Swal.fire({
+          position: "center-center",
+          icon: "success",
+          title: "Item Deleted Successfully",
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      }
+    },
+  });
+
   if (userIsLoading) return <Spinner />;
+
+  const handleDelete = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        deletUser(id);
+      }
+    });
+  };
+
+  const handleMakeAdmin = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "Do you want to make this user as admin??",
+      icon: "question",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, Please!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:3000/users/admin/${id}`, {
+          method: "PATCH",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            if (data.modifiedCount) {
+              Swal.fire({
+                position: "center-center",
+                icon: "success",
+                title: "Item Deleted Successfully",
+                showConfirmButton: false,
+                timer: 1500,
+              });
+            }
+          });
+      }
+    });
+  };
 
   return (
     <>
@@ -58,7 +128,7 @@ const Users = () => {
                       <td>
                         <button
                           className="p-4 inline-block rounded-xl cursor-pointer bg-gold border-gold"
-                          // onClick={() => handleMakeAdmin(user._id)}
+                          onClick={() => handleMakeAdmin(user._id)}
                         >
                           {user.role === "admin" ? (
                             <RiAdminFill
@@ -76,7 +146,7 @@ const Users = () => {
                       <td>
                         <button
                           className="p-4 inline-block rounded-xl cursor-pointer bg-error border-error"
-                          // onClick={() => handleDelete(user._id)}
+                          onClick={() => handleDelete(user._id)}
                         >
                           <RiDeleteBin5Line className="text-2xl text-white" />
                         </button>
